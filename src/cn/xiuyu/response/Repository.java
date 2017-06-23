d030ea052946951b0951efc307f1b9275ff37d1c
gexyuzz <gexyuzz@qq.com>
2017-06-23 17:55:35 +0800

    rename name

---
+++ src/cn/xiuyu/response/Repository.java
@@ -0,0 +1,15 @@
+package cn.xiuyu.response;
+
+import java.sql.SQLException;
+
+import cn.xiuyu.entity.PageEntity;
+import cn.xiuyu.entity.SortEntity;
+
+public interface Repository<T> {
+	public void insert(T t) throws IllegalArgumentException, IllegalAccessException, SQLException;
+	public T query(int id) throws SQLException, InstantiationException, IllegalAccessException;
+	public void delete(int id) throws SQLException;
+	public void update(T t) throws SQLException;
+	public PageEntity<T> QueryBypage(PageEntity<T> entity) throws SQLException, InstantiationException, IllegalAccessException;
+	public int Querycount() throws SQLException;
+}
